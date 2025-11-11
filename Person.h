86b95934dd0b4ad50bb66f15459827f2c79cd518
KhaledAbbas1074 <khalodwaleed123@gmail.com>
2025-11-11 09:58:33 +0200

    Implement Person class with basic functionality
    Added Person class with constructors and member functions.

--- Person.h
+++ Person.h
@@ -1,20 +1,23 @@
 #ifndef PERSON_H
 #define PERSON_H
-
+#include<iostream>
 #include <string>
 using namespace std;
 
-//#write Person class here
+class Person {
+private:
+    string name;
+    int id;
+public:
+    Person();
+    Person(string name,int id);
+
+    void display();
+    string getName();
+    int getId();
+    ~Person();
+};
 
 
 
-
-
-
-
-
-
-
-
-
-#endif
+#endif 
