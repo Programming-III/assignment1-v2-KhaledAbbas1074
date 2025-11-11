f6688ed741241fc9e49ae69648a96f7dc948f9d5
KhaledAbbas1074 <khalodwaleed123@gmail.com>
2025-11-11 09:55:48 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -1,22 +1,31 @@
+//
+// Created by khalo on 11/11/2025.
+//
+
 #ifndef COURSE_H
 #define COURSE_H
 
-#include <string>
+
+#include<string>
+#include<iostream>
+
 #include "Student.h"
 using namespace std;
-//#write your code here
+class Course {
+private:
+    string CourseCode;
+    string CourseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
+public:
+    Course();
+    Course(string CourseCode, string CourseName,int maxStudents);
+    void addStudent(const Student& student);
+    void displayCourseInfo();
+    ~Course();
+};
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-#endif
+#endif 
